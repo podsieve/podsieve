3866c99a7810751a2f82209a8d18773ab894187f
shyamvora <shyamvora91@gmail.com>
2025-04-01 15:01:48 -0400

    git commit -m "updated podcast page link issue"

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import TilesSection from '../components/TilesSection';
